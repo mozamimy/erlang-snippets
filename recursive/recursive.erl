c9f81cfdf529d6dc9759c9bda88b08a00ce58a0f
Moza USANE <mozamimy@quellencode.org>
2014-12-31 23:44:37 +0900

    Added sublist/2 and tail_sublist/2 to recursive.erl.

--- recursive/recursive.erl
+++ recursive/recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, fac_short/1, tail_fac/1, len/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1]).
+-export([fac/1, fac_short/1, tail_fac/1, len/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 -> N * fac(N-1).
@@ -33,3 +33,13 @@ reverse([H|T]) -> reverse(T) ++ [H].
 tail_reverse(L) -> tail_reverse(L,[]).
 tail_reverse([],Acc) -> Acc;
 tail_reverse([H|T],Acc) -> tail_reverse(T,[H|Acc]).
+
+sublist(_,0) -> [];
+sublist([],_) -> [];
+sublist([H|T],N) when N > 0 -> [H|sublist(T,N-1)].
+
+tail_sublist(L,N) -> reverse(tail_sublist(L,N,[])).
+tail_sublist(_,0,SubList) -> SubList;
+tail_sublist([],_,SubList) -> SubList;
+tail_sublist([H|T],N,SubList) when N > 0 ->
+  tail_sublist(T,N-1,[H|SubList]).
