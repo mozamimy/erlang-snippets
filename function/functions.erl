404e01f8a5beb9d6de206d4623bee9b8640955a3
Moza USANE <mozamimy@quellencode.org>
2014-12-31 20:10:01 +0900

    Improved functions.erl.

--- function/functions.erl
+++ function/functions.erl
@@ -7,3 +7,13 @@ greet(female, Name) ->
   io:format("Hello, Mrs. ~s!", [Name]);
 greet(_, Name) ->
   io:format("Hello, ~s!", [Name]).
+
+head([H|_]) -> H.
+
+second([_,X|_]) -> X.
+
+valid_time({Date = {Y,M,D}, Time = {H,Min,S}}) ->
+  io:format("The Date tuple (~p) says today is: ~p/~p/~p,~n", [Date,Y,M,D]),
+  io:format("The time tuple (~p) indicates: ~p:~p:~p.~n", [Time,H,Min,S]);
+valid_time(_) ->
+  io:format("Stop feeding me wrong data!~n").
