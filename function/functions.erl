bb7c592bdf15498cb3363a8b0b464dbd50f78349
Moza USANE <mozamimy@quellencode.org>
2014-12-31 16:41:28 +0900

    Added functions.erl.

---
+++ function/functions.erl
@@ -0,0 +1,9 @@
+-module(functions).
+-compile(export_all).
+
+greet(male, Name) ->
+  io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) ->
+  io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) ->
+  io:format("Hello, ~s!", [Name]).
