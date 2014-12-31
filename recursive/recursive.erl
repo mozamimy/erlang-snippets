c7ac6704971be832680ff1a5713d20b4d2f74541
Moza USANE <mozamimy@quellencode.org>
2014-12-31 23:29:41 +0900

    Added duplicate/2 and tail_duplicate/2 to recursive.erl.

--- recursive/recursive.erl
+++ recursive/recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, fac_short/1, tail_fac/1, len/1, tail_len/1]).
+-export([fac/1, fac_short/1, tail_fac/1, len/1, tail_len/1, duplicate/2, tail_duplicate/2]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 -> N * fac(N-1).
@@ -17,3 +17,12 @@ len([_|T]) -> 1 + len(T).
 tail_len(L) -> tail_len(L,0).
 tail_len([], Acc) -> Acc;
 tail_len([_|T], Acc) -> tail_len(T, Acc+1).
+
+duplicate(0,_) -> [];
+duplicate(N,Term) when N > 0 ->
+  [Term|duplicate(N-1, Term)].
+
+tail_duplicate(N,Term) -> tail_duplicate(N,Term,[]).
+tail_duplicate(0,_,List) -> List;
+tail_duplicate(N,Term,List) when N > 0 ->
+  tail_duplicate(N-1, Term, [Term|List]).
