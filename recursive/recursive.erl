702ae8afff96fe32fc813626bb03add096be952f
Moza USANE <mozamimy@quellencode.org>
2015-01-01 00:42:22 +0900

    Added quicksort/1 and lc_quicksort/1 to recursive.erl.

--- recursive/recursive.erl
+++ recursive/recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, fac_short/1, tail_fac/1, len/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, lenient_zip/2, tail_zip/2, tail_lenient_zip/2]).
+-export([fac/1, fac_short/1, tail_fac/1, len/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, lenient_zip/2, tail_zip/2, tail_lenient_zip/2, quicksort/1, lc_quicksort/1]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 -> N * fac(N-1).
@@ -59,3 +59,21 @@ tail_lenient_zip(Xs,Ys) -> reverse(tail_lenient_zip(Xs,Ys,[])).
 tail_lenient_zip([],_,Acc) -> Acc;
 tail_lenient_zip(_,[],Acc) -> Acc;
 tail_lenient_zip([X|Xs],[Y|Ys],Acc) -> tail_lenient_zip(Xs,Ys,[{X,Y}|Acc]).
+
+% quicksort
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+  {Smaller, Larger} = partition(Pivot,Rest,[],[]),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+partition(_,[],Smaller,Larger) -> {Smaller, Larger};
+partition(Pivot,[H|T],Smaller,Larger) ->
+  if H =< Pivot -> partition(Pivot,T,[H|Smaller],Larger);
+     H >  Pivot -> partition(Pivot,T,Smaller,[H|Larger])
+  end.
+
+lc_quicksort([]) -> [];
+lc_quicksort([Pivot|Rest]) ->
+  lc_quicksort([Smaller || Smaller <- Rest, Smaller =< Pivot])
+  ++ [Pivot] ++
+  lc_quicksort([Larger || Larger <- Rest, Larger > Pivot]).
