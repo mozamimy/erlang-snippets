f43a2ae2aece6ffc98b6eee84ca931f0ba993788
Moza USANE <mozamimy@quellencode.org>
2015-01-01 12:48:49 +0900

    Added some high-order functions to hhfuns.erl.

--- high-order/hhfuns.erl
+++ high-order/hhfuns.erl
@@ -24,3 +24,57 @@ a() ->
 
 b(F) ->
   "a/0's password is " ++ F().
+
+even(L) -> lists:reverse(even(L,[])).
+even([], Acc) -> Acc;
+even([H|T], Acc) when H rem 2 == 0 ->
+  even(T, [H|Acc]);
+even([_|T], Acc) -> even(T, Acc).
+
+old_man(L) -> lists:reverse(old_man(L,[])).
+old_man([], Acc) -> Acc;
+old_man([Person = {male, Age}|People], Acc) when Age > 60 ->
+  old_man(People, [Person|Acc]);
+old_man([_|People], Acc) -> old_man(People, Acc).
+
+filter(Pred, L) -> lists:reverse(filter(Pred, L, [])).
+filter(_, [], Acc) -> Acc;
+filter(Pred, [H|T], Acc) ->
+  case Pred(H) of
+    true -> filter(Pred, T, [H|Acc]);
+    false -> filter(Pred, T, Acc)
+  end.
+
+max([H|T]) -> max2(T,H).
+
+max2([], Max) -> Max;
+max2([H|T], Max) when H > Max -> max2(T,H);
+max2([_|T], Max) -> max2(T,Max).
+
+min([H|T]) -> min2(T,H).
+
+min2([], Min) -> Min;
+min2([H|T], Min) when H < Min -> min2(T,H);
+min2([_|T], Min) -> min2(T,Min).
+
+sum(L) -> sum(L,0).
+sum([], Sum) -> Sum;
+sum([H|T], Sum) -> sum(T,H+Sum).
+
+fold(_, Start, []) -> Start;
+fold(F, Start, [H|T]) -> fold(F,F(H,Start),T).
+
+reverse(L) ->
+  fold(fun(X, Acc) -> [X|Acc] end, [], L).
+
+map2(F, L) ->
+  reverse(fold(fun(X, Acc) -> [F(X)|Acc] end, [], L)).
+
+filter2(Pred, L) ->
+  F = fun(X, Acc) ->
+    case Pred(X) of
+      true -> [X|Acc];
+      false -> Acc
+    end
+  end,
+  reverse(fold(F,[],L)).
